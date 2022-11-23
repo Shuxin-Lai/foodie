1c3c2e9a2af06a57ea8ce062a3728491e6ccf75a
Laishuxin <15622971239@163.com>
2022-11-23 22:23:13 +0800

    feat: connect database

--- foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
@@ -1,12 +1,26 @@
 package com.imooc.controller;
 
+import com.imooc.mapper.StuMapper;
+import com.imooc.pojo.Stu;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+  @Autowired
+  private StuMapper stuMapper;
+
   @GetMapping("/ping")
   public Object hello() {
     return "hello world";
+  }
+
+  @GetMapping("/test_database")
+  public Object testDatabase() {
+    Stu stu = new Stu();
+    stu.setId(1);
+    Stu stu1 = stuMapper.selectByPrimaryKey(stu);
+    return stu1;
   }
 }
---
+++ foodie-dev-pojo/src/main/java/com/imooc/pojo/Stu.java
@@ -0,0 +1,104 @@
+package com.imooc.pojo;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import java.util.Date;
+
+public class Stu {
+    @Id
+    private Integer id;
+
+    /**
+     * 创建时间
+     */
+    @Column(name = "CREATED_TIME")
+    private Date createdTime;
+
+    /**
+     * 更新时间
+     */
+    @Column(name = "UPDATED_TIME")
+    private Date updatedTime;
+
+    private String name;
+
+    private Integer age;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取创建时间
+     *
+     * @return CREATED_TIME - 创建时间
+     */
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    /**
+     * 设置创建时间
+     *
+     * @param createdTime 创建时间
+     */
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    /**
+     * 获取更新时间
+     *
+     * @return UPDATED_TIME - 更新时间
+     */
+    public Date getUpdatedTime() {
+        return updatedTime;
+    }
+
+    /**
+     * 设置更新时间
+     *
+     * @param updatedTime 更新时间
+     */
+    public void setUpdatedTime(Date updatedTime) {
+        this.updatedTime = updatedTime;
+    }
+
+    /**
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return age
+     */
+    public Integer getAge() {
+        return age;
+    }
+
+    /**
+     * @param age
+     */
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
