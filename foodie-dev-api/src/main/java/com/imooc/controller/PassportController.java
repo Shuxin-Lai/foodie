6f0745696c7007cc6b3d61fcc257ea3e61307c88
Laishuxin <15622971239@163.com>
2022-11-24 23:55:59 +0800

    perf: improve user validation and queryUsernameIsExist

--- foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
@@ -1,24 +1,30 @@
 package com.imooc.controller;
 
 import com.imooc.service.UserService;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Size;
 
 @RestController
 @RequestMapping("/passport")
+@Validated
 public class PassportController {
   @Autowired
   private UserService userService;
 
   @Valid
   @GetMapping("/usernameIsExist")
-  public String queryUsernameIsExists(@RequestParam @Size(min = 0, max = 255) String username) {
-    return username;
+  public int queryUsernameIsExists(@RequestParam @Length(max = 255, min = 3) String username) {
+    if (userService.queryUsernameIsExists(username)) {
+      return 500;
+    }
+
+    return 200;
   }
 }
